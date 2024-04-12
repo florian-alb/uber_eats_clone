ea0a9f66c0290a58ff523a457bad514d1546b2e5
Nunu <fravalo.killian2@gmail.com>
2024-04-12 11:51:29 +0200

    Added UI/UX position, Deliver / take, cart, burger Menu, searchBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import NavbarAuth from "@/components/noLib/NavbarAuth.tsx";
+import Navbar from "@/components/noLib/Navbar.tsx";
+import LoginForm from "@/Page/LoginPage.tsx";
+import RegisterPage from "@/Page/RegisterPage.tsx";
+
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline text-blue-500">
-            Hello world!
-        </h1>
+        <div className="w-full h-full flex">
+            <Navbar />
+        </div>
     )
 }
