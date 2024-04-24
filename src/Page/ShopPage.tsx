1d3d17defd81e27627c34643deed73332b680734
DepressedNunu <110053182+DepressedNunu@users.noreply.github.com>
2024-04-24 16:02:55 +0200

    89 voir tous les restaurants (#2)
    * LoginPage.tsx RegisterPage.tsx done (only UI/UX, still need to do the routing).
    
    * Added UI/UX position, Deliver / take, cart, burger Menu, searchBar
    
    * Added ShopCard
    
    * Fixed React Router
    -Added Error Page
    - Fixed Img Path
    - Added Some css

---
+++ src/Page/ShopPage.tsx
@@ -0,0 +1,102 @@
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel.tsx";
+import {useEffect, useState} from "react";
+import Navbar from "@/components/noLib/Navbar.tsx";
+import ProductCard from "@/components/noLib/ProductCard.tsx";
+import ProductCardLong from "@/components/noLib/ProductCardLong.tsx";
+import {Icons} from "@/Page/MainPage.tsx";
+
+
+
+type Category = {
+    name: string
+    image: string
+}
+
+type products = {
+    name: string
+    description: string
+    price: number
+    image: string
+}
+
+type Shop = {
+    name: string,
+    id: string
+    image: string,
+    Category: Category
+    products: products[]
+}
+
+
+export default function ShopPage() {
+
+    const [shopInfo, setShopInfo] = useState<Shop | undefined>(undefined)
+
+    useEffect(() => {
+        retrieveShopInfo().then(r => r)
+    }, [])
+
+    async function retrieveShopInfo() {
+        await fetch("http://localhost:8080/shop/e26fbf62-0036-4cb7-8962-e6815cc91a93")
+            .then(res => res.json())
+            .then(({data}) => {
+                if (!data) {
+                    return
+                }
+                setShopInfo(data)
+            })
+    }
+
+
+
+    if (!shopInfo) return (
+        <div className="flex w-full h-full justify-center flex-col items-center">
+            <Icons.spinner className="h-24 w-24 stroke-1 text-ub-green animate-spin"/>
+        </div>
+    )
+
+    return (
+        <>
+            <Navbar/>
+            <div className="px-9 pt-20 min-w-screen m-auto h-auto">
+                <img
+                    src={shopInfo.image ? "https://i.pinimg.com/originals/2b/8d/34/2b8d3481fd0855dfb0608f3198fd8adc.jpg" : ""}
+                    alt="No Image Data"
+                     className="w-full h-72 object-cover rounded-xl"/>
+                <div className="mt-5">
+                    <h1 className="text-3xl font-bold">{shopInfo.name}</h1>
+                    <p className="text-gray-500">{shopInfo.Category.name}</p>
+                </div>
+            </div>
+            <div className="h-screen size-full px-9 pt-20 min-w-screen m-auto flex">
+                <div className='categoryListing w-1/5 h-full bg-gray-100 rounded-xl shadow-2xl'>
+                    {
+                        // TODO: Add the category listing here
+                    }
+                </div>
+                <div className="productListing w-4/5 h-full">
+                    <div className="font-medium text-2xl m-12">Article en vedettes</div>
+                    <Carousel className="pl-0 pt-5 m-12 max-w-8xl min-w-96">
+                        <div className="top-0">
+                            <CarouselPrevious />
+                            <CarouselNext />
+                        </div>
+                        <CarouselContent key={"carousel"}>
+                            {shopInfo.products.map((product, index) => (
+                                <CarouselItem className="basis-56"><ProductCard product={product} key={index}></ProductCard></CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                    <div className="m-12">
+                        <div className="font-medium text-2xl mt-24">Tous les articles</div>
+                        <div className="flex flex-wrap gap-10 min-w-96">
+                            {shopInfo.products.map((product, index) => (
+                                <ProductCardLong product={product} key={index}></ProductCardLong>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
