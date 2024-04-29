95171417ec87759deead0bc24c410f9d88212439
Nunu <fravalo.killian2@gmail.com>
2024-04-29 23:54:57 +0200

    - Added Default configuration for adding Items to cart - Can delete Items from the cart - Store data in the localStorage - added the "jotai" library to manage states between Components
    TODO: Auto Refresh Products in cart without having to refresh the page each time

--- src/Page/ShopPage.tsx
+++ src/Page/ShopPage.tsx
@@ -1,11 +1,14 @@
 import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel.tsx";
 import {useEffect, useState} from "react";
-import Navbar from "@/components/noLib/Navbar.tsx";
+import Navbar, {Item} from "@/components/noLib/Navbar.tsx";
 import ProductCard, {Product} from "@/components/noLib/ProductCard.tsx";
 import ProductCardLong from "@/components/noLib/ProductCardLong.tsx";
 import {Icons} from "@/Page/MainPage.tsx";
 import {useParams} from "react-router-dom";
 
+
+import {atomWithStorage} from "jotai/utils";
+import {useAtom} from "jotai";
 
 
 type Category = {
@@ -21,25 +24,38 @@ type Shop = {
     products: Product[]
 }
 
+//Retrieve localCart from the localStorage
+const localCart = atomWithStorage('cart', [] as Item[])
 
 export default function ShopPage() {
 
-    function addToCart(product : Product){
-        if (localStorage.getItem("cart") === null) localStorage.setItem("cart", JSON.stringify([] as Product[]))
-        const cart = JSON.parse(localStorage.getItem("cart") as string)
-        cart.push(product)
-        localStorage.setItem("cart", JSON.stringify(cart))
+    //Cart Management
+    const [cart, setCart] = useAtom(localCart)
+
+    // Shop Infos
+    const shopID = useParams()
+    const [shopInfo, setShopInfo] = useState<Shop | undefined>(undefined)
+
+    function addToCart(product: Product) {
+
+        if (cart.length > 0) {
+            console.log(cart[0].id === product.id)
+            if (product.id != cart[0].id) {
+                alert("Vous ne pouvez pas commander des produits de deux restaurants différents")
+                return
+            }
+        }
+        setCart([...cart, product] as Item[])
     }
 
 
-    const shopID = useParams()
-    console.log(shopID)
-    const [shopInfo, setShopInfo] = useState<Shop | undefined>(undefined)
-
+    //Retrieve the shop ID from the URL
     useEffect(() => {
         retrieveShopInfo().then(r => r)
     }, [])
 
+
+    //Retrieve the shop information from the API
     async function retrieveShopInfo() {
         await fetch(`http://localhost:8080/shop/${shopID.id}`)
             .then(res => res.json())
@@ -50,9 +66,6 @@ export default function ShopPage() {
                 setShopInfo(data)
             })
     }
-
-
-
     if (!shopInfo) return (
         <div className="flex w-full h-full justify-center flex-col items-center">
             <Icons.spinner className="h-24 w-24 stroke-1 text-ub-green animate-spin"/>
@@ -64,9 +77,9 @@ export default function ShopPage() {
             <Navbar/>
             <div className="px-9 pt-20 min-w-screen m-auto h-auto">
                 <img
-                    src={shopInfo.image ? "https://i.pinimg.com/originals/2b/8d/34/2b8d3481fd0855dfb0608f3198fd8adc.jpg" : ""}
+                    src={shopInfo.image ? "https://i.pinimg.com/originals/2b/8d/34/2b8d3481fd0855dfb0608f3198fd8adc.jpg" : "https://i.pinimg.com/originals/2b/8d/34/2b8d3481fd0855dfb0608f3198fd8adc.jpg"}
                     alt="No Image Data"
-                     className="w-full h-72 object-cover rounded-xl"/>
+                    className="w-full h-72 object-cover rounded-xl"/>
                 <div className="mt-5">
                     <h1 className="text-3xl font-bold">{shopInfo.name ? shopInfo.name : ""}</h1>
                     <p className="text-gray-500">{shopInfo.Category.name ? shopInfo.Category.name : ""}</p>
@@ -81,13 +94,15 @@ export default function ShopPage() {
                 <div className="productListing w-4/5 h-full">
                     <div className="font-medium text-2xl m-12">Article en vedettes</div>
                     <Carousel className="pl-0 pt-5 m-12 max-w-8xl min-w-96">
-                        <div >
-                            <CarouselPrevious />
-                            <CarouselNext />
+                        <div>
+                            <CarouselPrevious/>
+                            <CarouselNext/>
                         </div>
                         <CarouselContent key={"carousel"}>
                             {shopInfo.products.map((product, index) => (
-                                <CarouselItem className="basis-56"><ProductCard addToCart={() => addToCart(product)} product={product} key={index}></ProductCard></CarouselItem>
+                                <CarouselItem className="basis-56"><ProductCard addToCart={() => addToCart(product)}
+                                                                                product={product}
+                                                                                key={index}></ProductCard></CarouselItem>
                             ))}
                         </CarouselContent>
                     </Carousel>
@@ -95,7 +110,8 @@ export default function ShopPage() {
                         <div className="font-medium text-2xl mt-24">Tous les articles</div>
                         <div className="flex flex-wrap gap-10 min-w-96">
                             {shopInfo.products.map((product, index) => (
-                                <ProductCardLong addToCart={() => addToCart(product)} product={product} key={index}></ProductCardLong>
+                                <ProductCardLong addToCart={() => addToCart(product)} product={product}
+                                                 key={index}></ProductCardLong>
                             ))}
                         </div>
                     </div>
--- src/components/noLib/Navbar.tsx
+++ src/components/noLib/Navbar.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/sheet"
 import {Button} from "@/components/ui/button.tsx";
 import {Input} from "@/components/ui/input.tsx";
-import {SearchIcon} from "lucide-react";
+import {CircleMinus, SearchIcon} from "lucide-react";
 
 import {
     ToggleGroup,
@@ -19,14 +19,65 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
-import {useState} from "react";
 import {Link} from "react-router-dom";
 
+//Utils Jotai
+import {useAtom} from "jotai";
+import {atomWithStorage} from "jotai/utils";
+
+export type Item = {
+    name: string
+    price: string
+    quantity: number
+}
+
+const localCart = atomWithStorage('cart', [] as Item[])
+
 export default function Navbar() {
-    const [haveItems, setItems] = useState(false)
+
+    const [items, setItems] = useAtom(localCart)
+
+
+    function GetTotalPrice(): number {
+        let total = 0
+        for (let i = 0; i < items.length; i++) {
+            total += parseFloat(items[i].price)
+        }
+        return total
+    }
+
+    function deleteItem(index: number) {
+        return () => {
+            const newItems = [...items] //make a copy of the array since we can't mutate the state directly
+            newItems.splice(index, 1)
+            setItems(newItems)
+        }
+    }
 
     function cartContent() {
-
+        if (items.length === 0) return <div>Votre panier est vide !</div>;
+        console.log(items)
+        return (
+            <>
+                <div className="flex justify-between ">
+                    <div className="text-black font-bold">{items.length} article</div>
+                    <div className="text-black">
+                        Sous-total: <span className="text-black font-medium">{GetTotalPrice()}$</span></div>
+                </div>
+                <div className="overflow-scroll mb-20 border-y-2 py-2 flex flex-col gap-3">
+                    {items.map((item, index) => (
+                        <div key={index} className="flex justify-between items-center py-1  hover:duration-500 ">
+                            <div className="flex gap-2">
+                                <h2 className="font-medium text-black">{item.name}</h2>
+                                <div className="flex justify-center text-gray-900">{item.price} $</div>
+                            </div>
+                            <CircleMinus onClick={deleteItem(index)}
+                                         className="hover:text-red-700 duration-300"></CircleMinus>
+                        </div>
+                    ))}
+                </div>
+            </>
+        );
     }
 
     return (
@@ -40,11 +91,11 @@ export default function Navbar() {
                     <SheetContent className="bg-white border-none pt-14" side={"left"}>
                         <SheetHeader>
                             <Button asChild
-                                className="rounded-xl min-h-14 py-5 mx-5 bg-black text-white text-lg hover:bg-gray-950 duration-200">
+                                    className="rounded-xl min-h-14 py-5 mx-5 bg-black text-white text-lg hover:bg-gray-950 duration-200">
                                 <Link to={"/register"}>Inscription</Link>
                             </Button>
                             <Button asChild
-                                className="rounded-xl min-h-14 py-5 mx-5 bg-gray-200 text-lg duration-200 hover:bg-gray-300 mt-20">
+                                    className="rounded-xl min-h-14 py-5 mx-5 bg-gray-200 text-lg duration-200 hover:bg-gray-300 mt-20">
                                 <Link to={"/login"}>Connexion</Link>
                             </Button>
                             <SheetDescription className="flex flex-col gap-5 py-5 font-medium">
@@ -55,7 +106,8 @@ export default function Navbar() {
                         </SheetHeader>
                     </SheetContent>
                 </Sheet>
-                <Link to="/" className="ml-5 tracking-tight font-medium">Uber<span className="ml-2 font-bold">Eats</span>
+                <Link to="/" className="ml-5 tracking-tight font-medium">Uber<span
+                    className="ml-2 font-bold">Eats</span>
                 </Link>
             </div>
             <ToggleGroup type={"single"} className="flex bg-gray-300 rounded-full px-5 mx-5 h-10">
@@ -83,11 +135,25 @@ export default function Navbar() {
                     <img src="/src/assets/svg/cart.svg" alt="search"
                          className="min-w-6 min-h-6 max-h-8 max-w-8 rounded-full"/>
                     <div
-                        className="absolute min-w-4 mx-6 max-h-5 bg-ub-green top-3 rounded-full text-white text-xs flex justify-center items-center">0
+                        className="absolute min-w-4 mx-6 max-h-5 bg-ub-green top-3 rounded-full text-white text-xs flex justify-center items-center">{items.length}
                     </div>
                 </PopoverTrigger>
-                <PopoverContent className="flex justify-center mt-2.5 rounded-xl bg-white">
-                    <div className="text-center text-gray-500">{cartContent()}</div>
+                <PopoverContent className="mt-2.5 rounded-l bg-white h-1/4 w-full min-w-96 overflow-scroll">
+                    <div className="text-center text-gray-500">
+                        {cartContent()}
+                    </div>
+                    {items.length > 0 &&
+                        <div className="absolute left-0 bottom-0 h-24  w-full flex justify-center  bg-white">
+                            <div className="flex text-gray-900 justify-between w-full mx-10">
+                                <div className="">Sous-total:</div>
+                                <div className="text-black font-medium">{GetTotalPrice()} $</div>
+                            </div>
+                            <Button asChild
+                                    className="bg-black text-white rounded w-4/5 absolute bottom-0 mx-4 h-12 hover:bg-ub-dark"><Link
+                                to={"/checkout"} className="text-ub-dark">Commander</Link></Button>
+                        </div>
+                    }
+
                 </PopoverContent>
             </Popover>
             <div className="flex gap-2 mx-5">
